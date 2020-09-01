d505c0f1bef12ea9be6fb48ad0f2ca2deeec9c16
Sandra Leriche <sandra.leriche92@gmail.com>
2020-09-01 21:53:26 +1000

    final clean and csv export

--- scripts/property_type.R
+++ scripts/property_type.R
@@ -35,37 +35,64 @@ p_type_2017 <- p_type_2017[-1,]
 p_type_2018 <- p_type_2018[-1,]
 
 # add year 
-p_type_2010$year <- '01-01-2010'
-p_type_2011$year <- '01-01-2011'
-p_type_2012$year <- '01-01-2012'
-p_type_2013$year <- '01-01-2013'
-p_type_2014$year <- '01-01-2014'
-p_type_2015$year <- '01-01-2015'
-p_type_2016$year <- '01-01-2016'
-p_type_2017$year <- '01-01-2017'
-p_type_2018$year <- '01-01-2018'
+p_type_2010$year <- '2010'
+p_type_2011$year <- '2011'
+p_type_2012$year <- '2012'
+p_type_2013$year <- '2013'
+p_type_2014$year <- '2014'
+p_type_2015$year <- '2015'
+p_type_2016$year <- '2016'
+p_type_2017$year <- '2017'
+p_type_2018$year <- '2018'
 
 # merge datasets 
 p_type_2010_2018 <- rbind(p_type_2010,p_type_2011, p_type_2012, p_type_2013, p_type_2014, p_type_2015, p_type_2016, p_type_2017, p_type_2018)
 
 p_type_2010_2018 <- p_type_2010_2018 %>%
-  rename(county = NAME,
+  rename(county = 'NAME',
          'total_properties' = DP04_0006E,
          'house' = DP04_0007E,
-         '%_house'= DP04_0007PE,
-         '1_unit_attached' = DP04_0008E,
-         '%1_unit_attached' = DP04_0008PE,
-         '2_units' = DP04_0009E,
-         '%2_units' = DP04_0009PE,
-         '3_4_units' = DP04_0010E, 
-         '%3_4_units' = DP04_0010PE,
-         '5_9_units' = DP04_0011E,
-         '%5_9_units' = DP04_0011PE,
-         '10_19_units' = DP04_0012E, 
-         '%10_19_units' = DP04_0012PE,
-         '20_more_units' = DP04_0013E,
-         '%20_more_units' = DP04_0013PE)
+         'per_house'= DP04_0007PE,
+         'one_unit_attached' = DP04_0008E,
+         'per_one_unit_attached' = DP04_0008PE,
+         'two_units' = DP04_0009E,
+         'per_two_units' = DP04_0009PE,
+         'three_four_units' = DP04_0010E, 
+         'per_three_four_units' = DP04_0010PE,
+         'five_nine_units' = DP04_0011E,
+         'per_five_nine_units' = DP04_0011PE,
+         'mid_rise_apartments' = DP04_0012E, 
+         'per_mid_rise_apartments' = DP04_0012PE,
+         'high_rise_apartments' = DP04_0013E,
+         'per_high_rise_apartments' = DP04_0013PE)
 
-# TO DO: group by house, townhouse or apartment:
+# group by house, townhouse or apartment:
+# 1_detached = house, 1_unit_attached and 2_units = townhouse, 3_4, 5_9 = low_rise_apartments, 10_19 = mid_rise_apartments, 20_more = high_rise_apartments
+
+# Convert to numeric - identified one row that does not have values: 	Lake County, California - 2017
+
+p_type_2010_2018[,2:17] <- sapply(p_type_2010_2018[,2:17],as.numeric)
+
+# group by house, townhouse or apartment:
 # 1_detached = house, 1_unit_attached and 2_units = townhouse, 3_4, 5_9 = low_rise_apartments, 10_19 = med_rise_apartments, 20_more = high_rise_apartments
+
+p_type_2010_2018$townhouse <- p_type_2010_2018$one_unit_attached +     p_type_2010_2018$two_units
+
+p_type_2010_2018$per_townhouse <- p_type_2010_2018$per_one_unit_attached + p_type_2010_2018$per_two_units
+
+p_type_2010_2018$low_rise_apartments <- p_type_2010_2018$three_four_units + p_type_2010_2018$five_nine_units
+
+p_type_2010_2018$per_low_rise_apartments <- p_type_2010_2018$per_three_four_units + p_type_2010_2018$per_five_nine_units
+
+# keep relevant columns and re-order
+p_type_2010_2018 <- p_type_2010_2018[,c(1,17,2:4,18:21,13:16)]
+
+# Split name into county and state
+p_type_2010_2018 <- p_type_2010_2018 %>% 
+  separate(county, 
+           sep = ", ", 
+           into = c("county", "state"))
+
+# export csv
+#write.csv(p_type_2010_2018,"./property_type.csv", row.names = FALSE)
 
