bfed6fcb4511e67cdde890f08a62169a6411e84d
tianyanggao-uts <mr.gao.alexander@gmail.com>
2020-09-17 14:02:19 +1000

    update add violin plot

--- scripts/house_price_EDA.R
+++ scripts/house_price_EDA.R
@@ -5,7 +5,6 @@ library(janitor)
 library(lubridate)
 
 house_price <- read_csv(here("data","house_price","h_price_clean.csv"))
-
 skim(house_price)
 
 house_price %>% count(city,county)
@@ -41,11 +40,38 @@ ggplot(data = top_house_price, mapping = aes(median_price)) +
   facet_wrap(~county) +
   labs(title="Median price histogram for top 10 variant counties")
 
-rest =grouped_house_price %>% filter(!(county %in% counties))
+#rest =grouped_house_price %>% filter(!(county %in% counties))
 
 ggplot(data = grouped_house_price, mapping = aes(median_price)) +
   geom_histogram() + 
   facet_wrap(~county) +
   labs(title="Median price histogram for 54 counties")
 
+#box plot
 
+sanbernardino = house_price%>% filter(county =='san bernardino') %>%
+  mutate(year = as.factor(year))
+class(sanbernardino$year)
+ggplot(data = sanbernardino, mapping = aes(y=house_price, x = year)) +
+  geom_violin()
+
+house_price <- house_price%>% mutate(year = as.factor(year))
+
+ggplot(data = house_price, mapping = aes(y=house_price, x = year)) +
+  geom_violin() +
+  facet_wrap(~county)
+
+rest = house_price %>% filter(county %in% counties)
+count(rest, county)
+
+ggplot(data = rest, mapping = aes(y=house_price, x = year)) +
+  geom_violin() +
+  facet_wrap(~county)
+
+tier1 = c("san mateo","santa clara","marin")
+t1 = house_price %>% filter(county %in% tier1)
+count(t1, county)
+ggplot(data = t1, mapping = aes(y=house_price, x = year)) +
+  geom_violin() +
+  facet_wrap(~county) +
+  coord_flip()
