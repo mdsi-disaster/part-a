2962cf74d5b1035540b0dd0974348611a5dca4e1
Ivan Study <ivanstudy@192-168-1-108.tpgi.com.au>
2020-10-09 20:37:34 +1100

    more eq charts

--- scripts/ivan EDA scripts/eq_chart4.R
+++ scripts/ivan EDA scripts/eq_chart4.R
@@ -39,5 +39,51 @@ ggplot(data=kern_2019, aes(x=month, y=price)) +
        y = "House Price Index",
        x = "Month")
 
+## version 2 chart - convert to date (filter by 6 months either side of EQ event)
+kern2 <- h_county %>%
+  filter(year == 2018 | year == 2019, county == "kern") %>%
+  mutate(temp = paste(month.name[month],year,sep=" "))
 
+kern2_2 <- kern2 %>%
+  mutate(date = as.yearmon(temp)) %>%
+  filter(date >= 2018 & date < 2019.05)
 
+rect2 <- data.frame(xmin=2018.472, xmax=2018.528, ymin=-Inf, ymax=Inf)
+
+ggplot(data=kern2_2, aes(x=date, y=price)) +
+  geom_rect(data=rect2, aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax),
+            fill="#f7bb97",
+            alpha=0.6,
+            inherit.aes = FALSE) +
+  geom_line() +
+  geom_point() +
+  scale_x_continuous(breaks = kern2_2$date,
+                     labels = as.yearmon(kern2_2$date)) +
+  scale_y_continuous(label = dollar_format(), breaks = seq(175000,190000,2500)) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        panel.grid.minor.x = element_blank()) +
+  labs(title = "House price index for Kern county in 2019",
+       subtitle = "5th July 2019 Ridgecrest earthquake highlighted\n(7.1 Magnitude)",
+       y = "House Price Index",
+       x = "")
+
+## version 3 - house price growth by %
+kern3 <- kern2_2 %>%
+  mutate(growth = round(price/175903.6 - 1,digits = 4))
+
+ggplot(data=kern3, aes(x=date, y=growth)) +
+  geom_rect(data=rect2, aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax),
+            fill="#f7bb97",
+            alpha=0.6,
+            inherit.aes = FALSE) +
+  geom_line() +
+  geom_point() +
+  scale_x_continuous(breaks = kern3$date,
+                     labels = as.yearmon(kern3$date)) +
+  scale_y_continuous(label = percent_format(), breaks = seq(0,0.07,0.01)) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        panel.grid.minor.x = element_blank()) +
+  labs(title = "House price index for Kern county in 2019",
+       subtitle = "5th July 2019 Ridgecrest earthquake highlighted\n(7.1 Magnitude)",
+       y = "House Price Index",
+       x = "")
--- scripts/ivan EDA scripts/eq_chart5.R
+++ scripts/ivan EDA scripts/eq_chart5.R
@@ -41,6 +41,55 @@ ggplot(data=napa_2019, aes(x=month, y=price)) +
             alpha=0.5,
             inherit.aes = FALSE) +
   labs(title = "House price index for Napa county in 2014",
-       subtitle = "24th Aug 2019 Napa earthquake highlighted\n(6.0 Magnitude)",
+       subtitle = "24th Aug 2014 Napa earthquake highlighted\n(6.0 Magnitude)",
        y = "House Price Index",
        x = "Month")
+
+## version 2 chart - convert to date (filter by 6 months either side of EQ event)
+napa_2 <- h_county %>%
+  filter(year == 2014 | year == 2015, county == "napa") %>%
+  mutate(temp = paste(month.name[month],year,sep=" "))
+
+napa3 <- napa_2 %>%
+  mutate(date = as.yearmon(temp)) %>%
+  filter(date > 2014.05 & date < 2015.1)
+
+rect2 <- data.frame(xmin=2014.555, xmax=2014.611, ymin=-Inf, ymax=Inf)
+
+ggplot(data=napa3, aes(x=date, y=price)) +
+  geom_rect(data=rect2, aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax),
+            fill="#f7bb97",
+            alpha=0.6,
+            inherit.aes = FALSE) +
+  geom_line() +
+  geom_point() +
+  scale_x_continuous(breaks = napa3$date,
+                     labels = as.yearmon(napa3$date)) +
+  scale_y_continuous(label = dollar_format(), breaks = seq(620500,657000,7000)) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        panel.grid.minor.x = element_blank()) +
+  labs(title = "House price index for Napa county in 2014",
+       subtitle = "24th Aug 2014 Napa earthquake highlighted\n(6.0 Magnitude)",
+       y = "House Price Index",
+       x = "")
+
+## version 3 - house price growth by %
+napa4 <- napa3 %>%
+  mutate(growth = round(price/620619.5 - 1, digits=4))
+
+ggplot(data=napa4, aes(x=date, y=growth)) +
+  geom_rect(data=rect2, aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax),
+            fill="#f7bb97",
+            alpha=0.6,
+            inherit.aes = FALSE) +
+  geom_line() +
+  geom_point() +
+  scale_x_continuous(breaks = napa3$date,
+                     labels = as.yearmon(napa3$date)) +
+  scale_y_continuous(label = percent_format(), breaks = seq(0,0.07,0.01)) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        panel.grid.minor.x = element_blank()) +
+  labs(title = "House price index movement before and after",
+       subtitle = "the 24th Aug 2014 Napa earthquake\n(6.0 Magnitude)",
+       y = "House Price Index Gain\nas of Feb 2014",
+       x = "")
